134a1882edc2b5dbb6d35e6d7eac93ace757c6db
Fatmanur Temel <fatmanurtml@gmail.com>
2024-01-21 15:04:56 +0300

    create home page

--- app/page.js
+++ app/page.js
@@ -1,95 +1,77 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import depratureTo from '@/images/to.svg'
+import depratureFrom from '@/images/from.svg'
+import calendar from '@/images/calendar.svg'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className={styles.home_container}>
+      <div>
+        <h1 className={styles.home_title}>Uçmaya Hazır Mısın?</h1>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles}>
+        <form action="#" method='post'>
+          <div className='row-2'>
+            <div>
+              <label htmlFor="">
+                <Image 
+                  src={depratureFrom}
+                  width="30"
+                  height="24" 
+                  alt="deprature from icon" />
+                Nereden
+              </label>
+              <input type="text" />
+            </div>
+            <div>
+              <label htmlFor="">
+                <Image 
+                  src={depratureTo}
+                  width="30"
+                  height="24" 
+                  alt="deprature to icon" />
+                Nereye
+              </label>
+              <input type="text" />
+            </div>
+          </div>
+          <div className='row-2'>
+          <div>
+              <label htmlFor="">
+                <Image 
+                  src={calendar}
+                  width="30"
+                  height="24" 
+                  alt="calendar icon" />
+                Gidiş Tarihi
+              </label>
+              <input type="text" />
+            </div>
+            <div>
+              <label htmlFor="">
+              <Image 
+                  src={calendar}
+                  width="30"
+                  height="24" 
+                  alt="calendar icon" />
+                Dönüş Tarihi
+              </label>
+              <input type="text" />
+              <label htmlFor="">
+                <input type="checkbox" />
+                Tek Yön
+              </label>
+            </div>
+          </div>
+          <div className='row-1'>
+            <button type='button'>
+              <span>Uçuş Ara</span>
+              <i></i>
+            </button>
+          </div>
+        </form>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
