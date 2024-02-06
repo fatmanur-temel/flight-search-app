ca3fee4c13d4eed12db3070f63e60757e7395b8a
Fatmanur Temel <fatmanurtml@gmail.com>
2024-02-06 04:01:43 +0300

    update results page

--- app/results/[rlt_id]/page.js
+++ app/results/[rlt_id]/page.js
@@ -2,19 +2,19 @@ import React from 'react'
 import Image from 'next/image'
 import FlightTable from '@/components/FlightTable'
 import styles from './styles.module.css'
-import depratureTo from '@/images/to.svg'
-import depratureFrom from '@/images/from.svg'
-import calendar from '@/images/calendar.svg'
+import depratureTo from '@/public/images/to.svg'
+import depratureFrom from '@/public/images/from.svg'
+import calendar from '@/public/images/calendar.svg'
 import Flights from '@/mocks/flights.json'
 
 function Result() {
-    // console.log(Flights.flights[0]);
-    return (
-      <div>
-        <div>
-          {/* Flight search area */}
-          <div className="container">
-            <div className="col-lg11 col-md-12">
+  return (
+    <div>
+      <div className={styles.search_area}>
+        {/* Flight search area */}
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-10 col-12">
               <div className="row">
                 <div className="col-lg-6 col-12">
                   <div className="row">
@@ -22,7 +22,7 @@ function Result() {
                       <label htmlFor="">
                         <Image
                           src={depratureFrom}
-                          width="29"
+                          width="20"
                           height="20"
                           alt="deprature from icon" />
                         <span>Nereden</span>
@@ -33,7 +33,7 @@ function Result() {
                       <label htmlFor="">
                         <Image
                           src={depratureTo}
-                          width="29"
+                          width="20"
                           height="20"
                           alt="deprature to icon" />
                         <span>Nereye</span>
@@ -48,7 +48,7 @@ function Result() {
                       <label htmlFor="">
                         <Image
                           src={calendar}
-                          width="29"
+                          width="20"
                           height="20"
                           alt="calendar icon" />
                         <span>Gidiş Tarihi</span>
@@ -59,7 +59,7 @@ function Result() {
                       <label htmlFor="">
                         <Image
                           src={calendar}
-                          width="29"
+                          width="20"
                           height="20"
                           alt="calendar icon" />
                         <span>Dönüş Tarihi</span>
@@ -70,27 +70,33 @@ function Result() {
                 </div>
               </div>
             </div>
-            <div className="col-lg-1 col-md-12 col-12">
+            <div className="col-lg-2 col-12">
               <button type='button' className={styles.search_btn}>
                 <span>Uçuş Ara</span>
               </button>
             </div>
           </div>
         </div>
-        <div className="container">
-          {/* search result area */}
-          <div className="row">
-            <div className="col-lg-6">
-              <div className="search-header">
-                <span>Gidiş</span>
-              </div>
-              <FlightTable flights={Flights.flights} />
+      </div>
+      <div className="container mt-5">
+        {/* search result area */}
+        <div className="row">
+          <div className="col-lg-6">
+            <div className={`${styles.search_header} ${styles.yellow}`}>
+              <span>Gidiş</span>
             </div>
-            <div className="col-lg-6"></div>
+            <FlightTable flights={Flights.flights} />
+          </div>
+          <div className="col-lg-6">
+            <div className={`${styles.search_header} ${styles.green}`}>
+              <span>Dönüş</span>
+            </div>
+            <FlightTable flights={Flights.flights} />
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
 
-  export default Result
+export default Result
--- components/FlightTable/index.js
+++ components/FlightTable/index.js
@@ -1,42 +1,143 @@
 'use client'
-import React, { useState, useMemo } from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import styles from "./styles.module.css"
-import THY from "@/images/thy-logo.png"
-import AJET from "@/images/anadolujet-logo.png"
-import PGS from "@/images/pegasus-logo.png"
 
-function FlightTable({flights}) {
-    console.log(flights[0].id)
+const FlightTable = ({ flights }) => {
+    const [sortConfig, setSortConfig] = useState({
+        key: '',
+        direction: 'ascending',
+    });
+
+    const requestSort = (key) => {
+        let direction = 'ascending';
+        if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+            direction = 'descending';
+        }
+        setSortConfig({ key, direction });
+    };
+
+    const calculateFlightDuration = (departureTime, arrivalTime) => {
+        const [departureHours, departureMinutes] = departureTime.split(':').map(Number);
+        const [arrivalHours, arrivalMinutes] = arrivalTime.split(':').map(Number);
+
+        let hoursDiff = arrivalHours - departureHours;
+        let minutesDiff = arrivalMinutes - departureMinutes;
+
+        if (minutesDiff < 0) {
+            minutesDiff += 60;
+            hoursDiff--;
+        }
+
+        return hoursDiff * 60 + minutesDiff; // Süreyi dakika cinsine çevir
+    };
+
+    const getClassNamesFor = (name) => {
+        if (!sortConfig.key) {
+            return;
+        }
+
+        return sortConfig.key === name ? sortConfig.direction : undefined;
+    };
+
+    const sortedFlights = [...flights].sort((a, b) => {
+        if (sortConfig.key === 'flightDuration') {
+            const durationA = calculateFlightDuration(a.departureTime, a.arrivalTime);
+            const durationB = calculateFlightDuration(b.departureTime, b.arrivalTime);
+
+            if (durationA < durationB) {
+                return sortConfig.direction === 'ascending' ? -1 : 1;
+            }
+            if (durationA > durationB) {
+                return sortConfig.direction === 'ascending' ? 1 : -1;
+            }
+            return 0;
+        }
+
+        else if (sortConfig.key) {
+            const aValue = a[sortConfig.key];
+            const bValue = b[sortConfig.key];
+
+            if (sortConfig.direction === 'ascending') {
+                return aValue.localeCompare(bValue, undefined, { numeric: true });
+            } else {
+                return bValue.localeCompare(aValue, undefined, { numeric: true });
+            }
+        }
+        return 0;
+    });
+
     return (
-        <table>
+        <table className={styles.flight_table}>
             <thead>
                 <tr>
-                    <th>Havayolu</th>
-                    <th>Kalkış Saati</th>
-                    <th>Uçuş Süresi</th>
-                    <th>Varış Saati</th>
-                    <th>Fiyat</th>
+                    <th>
+                        <span>Havayolu</span>
+                    </th>
+                    <th onClick={() => requestSort('departureTime')} className={getClassNamesFor('departureTime')}>
+                        <span className={styles.sort_icons}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="t-6 bi bi-caret-up-fill" viewBox="0 0 16 16">
+                                <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="b-6 bi bi-caret-down-fill" viewBox="0 0 16 16">
+                                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                            </svg>
+                        </span>
+                        <span>Kalkış Saati</span>
+                    </th>
+                    <th onClick={() => requestSort('flightDuration')} className={getClassNamesFor('flightDuration')}>
+                        <span className={styles.sort_icons}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="t-6 bi bi-caret-up-fill" viewBox="0 0 16 16">
+                                <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="b-6 bi bi-caret-down-fill" viewBox="0 0 16 16">
+                                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                            </svg>
+                        </span>
+                        <span>Uçuş Süresi</span>
+                    </th>
+                    <th onClick={() => requestSort('arrivalTime')} className={getClassNamesFor('arrivalTime')}>
+                        <span className={styles.sort_icons}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="t-6 bi bi-caret-up-fill" viewBox="0 0 16 16">
+                                <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="b-6 bi bi-caret-down-fill" viewBox="0 0 16 16">
+                                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                            </svg>
+                        </span>
+                        <span>Varış Saati</span>
+                    </th>
+                    <th onClick={() => requestSort('price')} className={getClassNamesFor('price')}>
+                        <span className={styles.sort_icons}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="t-6 bi bi-caret-up-fill" viewBox="0 0 16 16">
+                                <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="b-6 bi bi-caret-down-fill" viewBox="0 0 16 16">
+                                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                            </svg>
+                        </span>
+                        <span>Fiyat</span>
+                    </th>
                 </tr>
             </thead>
             <tbody className={styles.table_body}>
-                {flights.map((flight) => (
+                {sortedFlights.map((flight) => (
                     <tr key={flight.id}>
                         <td>
-                        <Image
-                          src={THY}
-                          width="25"
-                          height="25"
-                          loading='lazy'
-                          alt="company logo" />
-                            <h4>{flight.airline}</h4>
+                            <Image
+                                src={flight.airlineLogo}
+                                width="25"
+                                height="25"
+                                loading='lazy'
+                                alt="company logo" />
+                            <p>{flight.airline}</p>
                         </td>
                         <td>
                             <p>{flight.departureTime}</p>
                             <p>{flight.from.short}</p>
                         </td>
                         <td>
-                            <p>{flight.flightDuration}</p>
+                            <p>{calculateFlightDuration(flight.departureTime, flight.arrivalTime)} dk</p>
                             <div className={styles.transmission}>
                                 <span className={styles.line_arrow}></span>
                             </div>
@@ -46,19 +147,19 @@ function FlightTable({flights}) {
                             <p>{flight.to.short}</p>
                         </td>
                         <td>
-                            <p>{flight.price}</p>
+                            <p>{flight.price} TL</p>
                             <button>Detay</button>
                         </td>
                     </tr>
                 ))}
             </tbody>
-            {/* <tfood>
+            <tfood>
                 <tr role="row">
                     <td colSpan="11" data-column="0">
                         <h3 className="no-flight-found">Kriterlerinize uygun uçuş bulunamamıştır.</h3>
                     </td>
                 </tr>
-            </tfood> */}
+            </tfood>
         </table>
     )
 }
